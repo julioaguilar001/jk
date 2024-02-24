67d6c1f12f1a08b2e769b38ac9ff9306c63ca7e0
julioaguilar001 <159965591+julioaguilar001@users.noreply.github.com>
2024-02-23 19:34:17 -0600

    cambios
    mejoras

--- scrip.js
+++ scrip.js
@@ -1,11 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let currentSlide = 0;
+    const slides = document.querySelectorAll(".slide");
+    const totalSlides = slides.length;
+
+    function showSlide(index) {
+        if (index < 0) {
+            currentSlide = totalSlides - 1;
+        } else if (index >= totalSlides) {
+            currentSlide = 0;
+        } else {
+            currentSlide = index;
+        }
+
+        slides.forEach((slide) => {
+            slide.style.display = "none";
+        });
+
+        slides[currentSlide].style.display = "block";
+    }
+
+    function nextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    function prevSlide() {
+        showSlide(currentSlide - 1);
+    }
+
+    // Mostrar el primer slide al cargar la página
+    showSlide(currentSlide);
+
+    // Agregar eventos para los botones de siguiente y anterior
+    document.getElementById("prevBtn").addEventListener("click", prevSlide);
+    document.getElementById("nextBtn").addEventListener("click", nextSlide);
+});
+
+
+
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
 
-abrir,addEventListener("click", () =>{
-    nav.classList.add("visible");
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
 })
-cerrar.addEventListener("click", () =>{
-    nav.classList.add("visible");
-})
+
+
